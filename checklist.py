f24d29a2d19ce279824398c5fd77772655a5a343
marco tanda <mtanda@umd.edu>
2018-05-09 19:50:05 -0400

    hi

--- checklist.py
+++ checklist.py
@@ -9,17 +9,17 @@ with open("merged-withcounts.csv",'r') as infile, open("checklist.csv", 'w') as 
     writer.writerow(col.split(','))
     for line in infile.readlines():
         line = line.strip().split('\t')
-        haslowercase = 0
-        hasuppercase = 0
-        hasnum = 0
-        hasspecial = 0
+        haslowercase = False
+        hasuppercase = False
+        hasnum = False
+        hasspecial = False
         if re.search(r'[a-z]', line[1]):
-            haslowercase = 1
+            haslowercase = True
         if re.search(r'[A-Z]', line[1]):
-            hasuppercase = 1
+            hasuppercase = True
         if re.search(r'[\d]', line[1]):
-            hasnum = 1
+            hasnum = True
         if re.search(r'[\`\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\|\]\}\[\{\'\"\;\:\/\\\?\.\>\,\<\ ]', line[1]):
-            hasspecial = 1
+            hasspecial = True
         sub = (line[0]+',' +line[1]+',' +str(haslowercase)+','+str(hasuppercase)+','+str(hasnum)+','+str(hasspecial))
         writer.writerow(sub.split(','))
