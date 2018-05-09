e7673689a2235a39b8cb89f6cd7bd3114636703a
marco tanda <mtanda@umd.edu>
2018-05-09 19:56:03 -0400

    fuccckkk

--- checklist.py
+++ checklist.py
@@ -1,25 +1,26 @@
 import os
 import re
 import csv
-
-with open("merged-withcounts.csv",'r') as infile, open("checklist.csv", 'w') as outfile:
-	#with open("HACS408V-netflow-host1-ssh.txt",'r') as infile, open("cleannetflowh1.csv", 'w') as outfile:   merged-withcounts
-    writer = csv.writer(outfile, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
-    col = ('count'+',' +'password'+',' +'lowercase'+','+'uppercase'+','+'number'+','+'special_char')
-    writer.writerow(col.split(','))
-    for line in infile.readlines():
-        line = line.strip().split('\t')
-        haslowercase = False
-        hasuppercase = False
-        hasnum = False
-        hasspecial = False
-        if re.search(r'[a-z]', line[1]):
-            haslowercase = True
-        if re.search(r'[A-Z]', line[1]):
-            hasuppercase = True
-        if re.search(r'[\d]', line[1]):
-            hasnum = True
-        if re.search(r'[\`\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\|\]\}\[\{\'\"\;\:\/\\\?\.\>\,\<\ ]', line[1]):
-            hasspecial = True
-        sub = (line[0]+',' +line[1]+',' +str(haslowercase)+','+str(hasuppercase)+','+str(hasnum)+','+str(hasspecial))
-        writer.writerow(sub.split(','))
+def fuckoffconnor(csvfile):
+	with open(csvfile,'r') as infile, open("checklist.csv", 'w') as outfile:
+		#with open("HACS408V-netflow-host1-ssh.txt",'r') as infile, open("cleannetflowh1.csv", 'w') as outfile:   merged-withcounts
+	    writer = csv.writer(outfile, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
+	    col = ('count'+',' +'password'+',' +'lowercase'+','+'uppercase'+','+'number'+','+'special_char')
+	    writer.writerow(col.split(','))
+	    for line in infile.readlines():
+	        line = line.strip().split('\t')
+	        haslowercase = False
+	        hasuppercase = False
+	        hasnum = False
+	        hasspecial = False
+	        if re.search(r'[a-z]', line[1]):
+	            haslowercase = True
+	        if re.search(r'[A-Z]', line[1]):
+	            hasuppercase = True
+	        if re.search(r'[\d]', line[1]):
+	            hasnum = True
+	        if re.search(r'[\`\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\|\]\}\[\{\'\"\;\:\/\\\?\.\>\,\<\ ]', line[1]):
+	            hasspecial = True
+	        sub = (line[0]+',' +line[1]+',' +str(haslowercase)+','+str(hasuppercase)+','+str(hasnum)+','+str(hasspecial))
+	        writer.writerow(sub.split(','))
+fuckoffconnor("merged-withcounts.csv")
--- complexity.py
+++ complexity.py
@@ -1,8 +1,8 @@
 import os
 import re
 import csv
-
-with open("merged-withcounts.csv",'r') as infile, open("complexity.csv", 'w') as outfile:
+def fuuuuuuuckkkkkkk(csvfile)
+with open(csvfile,'r') as infile, open("complexity.csv", 'w') as outfile:
     #with open("HACS408V-netflow-host1-ssh.txt",'r') as infile, open("cleannetflowh1.csv", 'w') as outfile:   merged-withcounts
     writer = csv.writer(outfile, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
     for line in infile.readlines():
@@ -21,4 +21,4 @@ with open("merged-withcounts.csv",'r') as infile, open("complexity.csv", 'w') as
         writer.writerow(row.split(','))
         #print(comp, line[1])
 
-        
+fuuuuuuuckkkkkkk("merged-withcounts.csv")      
